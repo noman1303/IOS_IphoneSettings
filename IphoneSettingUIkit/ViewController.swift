d69ef6989bcc9e51a455b23602ea921e0e3e8991
noman1303 <belimnoman1303@gmail.com>
2026-02-19 18:17:43 +0530

    Designs

--- IphoneSettingUIkit/ViewController.swift
+++ IphoneSettingUIkit/ViewController.swift
@@ -10,26 +10,35 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var FCard: UIStackView!
+    @IBOutlet weak var SecondCard: UIStackView!
     @IBOutlet weak var WifiBorder: UIView!
+    @IBOutlet weak var nameCard: UIView!
     @IBOutlet weak var BattryBorder: UIView!
+    @IBOutlet weak var ThirdCard: UIStackView!
+    @IBOutlet weak var demo: UIView!
+    @IBOutlet weak var NotificationUiVIew: UIView!
     @IBOutlet weak var AiroplanBorder: UIView!
     @IBOutlet weak var PersonlaHostpotBorder: UIView!
     @IBOutlet weak var BlueToothBorder: UIView!
     @IBOutlet weak var MobileServiceBorder: UIView!
+    @IBOutlet weak var GenralDash: UIView!
+    @IBOutlet weak var AccesDash: UIView!
+    @IBOutlet weak var DisDash: UIView!
+    @IBOutlet weak var ContDash: UIView!
+    @IBOutlet weak var CamDash: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        FCard.layer.cornerRadius = 10
-        FCard.backgroundColor = UIColor.white
-        FCard.layer.borderColor = UIColor.black.cgColor
-        FCard.layer.borderWidth = 1
+        FCard.layer.cornerRadius = 20
+        SecondCard.layer.cornerRadius = 20
+        SecondCard.layer.cornerRadius = 10
+        NotificationUiVIew.layer.cornerRadius = 10
         WifiBorder.backgroundColor = UIColor.gray
         BattryBorder.backgroundColor = UIColor.gray
         PersonlaHostpotBorder.backgroundColor = UIColor.gray
         BlueToothBorder.backgroundColor = UIColor.gray
         MobileServiceBorder.backgroundColor = UIColor.gray
         AiroplanBorder.backgroundColor = UIColor.gray
+        GenralDash.backgroundColor = UIColor.gray
     }
-
-
 }
 
